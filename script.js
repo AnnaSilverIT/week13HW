dec52e1e2f4478209e85ccac1ef8b3f4482f0677
AnnaSilverIT <student291297@gmail.com>
2024-09-07 12:33:07 +0300

    fix js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const btn = document.querySelector('.button');
-const input = document.querySelector('#input');
-const result = document.querySelector('.result')
+const button = document.querySelector('.button');
+const input = document.querySelector('#input').value;
+const result = document.querySelector('.result');
 
-// const function count() {
-    
-//   }
-btn.onclick = () => {
+button.onclick = () => {
+  const currentDate = Date.now();
+  let birthDate = Date.parse(input);
+  console.log(currentDate);
+  console.log(birthDate);
+  let diffInMS = birthDate.Date.parse - currentDate;
+  let diffInDays = (diffInMS / (1000*60*60*24))
   let newDate = document.createElement('p');
   newDate.className = 'newDate'
-  newDate.innerText = 'До вашего дня рождения осталось:' 
+  newDate.innerText = 'До вашего дня рождения осталось:' + diffInDays + 'дней!'
   input.value = ''
   result.appendChild(newDate)
   }
