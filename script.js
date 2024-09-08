4ef17dfe1b46c5c6ab2b7649fb2d5d0a549a0e76
AnnaSilverIT <student291297@gmail.com>
2024-09-08 21:20:31 +0300

    fix js part

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const button = document.querySelector('.button');
-const input = document.querySelector('#input').value;
+const input = document.querySelector('#input');
 const result = document.querySelector('.result');
 
 button.onclick = () => {
   const currentDate = Date.now();
-  let birthDate = Date.parse(input);
+  let inputValue = input.value
+  let birthDate = Date.parse(inputValue);
   console.log(currentDate);
   console.log(birthDate);
-  let diffInMS = birthDate.Date.parse - currentDate;
-  let diffInDays = (diffInMS / (1000*60*60*24))
+  let diffInMS = birthDate - currentDate;
+  let diffInDays = Math.round(diffInMS / (1000*60*60*24))
   let newDate = document.createElement('p');
+  console.log(diffInDays)
   newDate.className = 'newDate'
-  newDate.innerText = 'До вашего дня рождения осталось:' + diffInDays + 'дней!'
+  newDate.innerText = 'До вашего дня рождения осталось: ' + diffInDays + ' дней!'
   input.value = ''
   result.appendChild(newDate)
   }
